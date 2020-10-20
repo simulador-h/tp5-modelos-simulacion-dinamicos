f2b3ba2fda4afe703bcd66eaee3e46e5b4ab13a9
Mickaela Pizarro <micka.pizarro@gmail.com>
2020-10-19 22:02:45 -0300

    desviacion

--- src/helpers/estadistics.ts
+++ src/helpers/estadistics.ts
@@ -4,10 +4,11 @@ export function media(dia: number, mediaAnterior: number, ingreso: number) {
   suma = (1 / dia * ((dia - 1) * mediaAnterior + ingreso));
   return Number(suma.toFixed(4));
 }
-export function desviacionProm(dia: number, desviacionAnterior: number, media: number, ingreso: number) {
-  let suma = 0;
-  const desviacion = Math.abs(ingreso - media);
+export function desviacion(dia: number, varianzaAnterior: number,
+  mediaAnterior: number, media: number, ingreso: number) {
+  let varianza = 0;
   // eslint-disable-next-line no-mixed-operators
-  suma = (1 / dia * ((dia - 1) * desviacionAnterior + desviacion));
-  return Number(suma.toFixed(4));
+  varianza = varianzaAnterior + (ingreso - mediaAnterior) * (ingreso - media);
+  const desviacion = Math.sqrt(varianza / dia);
+  return Number(desviacion.toFixed(4));
 }
